1e97067e42cf15e6c55e315e4f1a58457db8aa0e
Bruno Renié <brutasse@gmail.com>
2015-10-20 14:46:01 +0200

    Update deps and cljsbuild definition

--- project.clj
+++ project.clj
@@ -5,37 +5,38 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :aot :all
   :main org.spootnik.warp
-  :plugins [[lein-cljsbuild "1.0.4"]]
+  :plugins [[lein-cljsbuild "1.1.0"]]
+  :clean-targets ^{:protect false} [:target-path
+                                    "resources/public/warp"]
   :cljsbuild {
     :builds [{:id "om-warp"
               :source-paths ["src/org/spootnik/om_warp"]
-              :compiler {
-                :main org.spootnik.om_warp.app
-                :output-to "resources/public/warp/app.js"
-                :output-dir "resources/public/warp"
-                :asset-path "/warp"
-                :optimizations :none
-                :source-map true}}]}
-  :dependencies [[org.clojure/clojure           "1.6.0"]
+              :compiler {:output-to "resources/public/warp/app.js"
+                         :output-dir "resources/public/warp"
+                         :asset-path "/warp"
+                         :optimizations :none
+                         :pretty-print false}
+              :main org.spootnik.om_warp.app}]}
+  :dependencies [[org.clojure/clojure           "1.7.0"]
                  [org.clojure/core.async        "0.1.346.0-17112a-alpha"]
                  [org.clojure/tools.logging     "0.3.1"]
                  [org.clojure/tools.cli         "0.3.1"]
-                 [org.clojure/data.json         "0.2.5"]
                  [org.clojure/data.codec        "0.1.0"]
                  [clj-yaml                      "0.4.0"]
-                 [compojure                     "1.3.2"]
-                 [cc.qbits/jet                  "0.5.4"]
-                 [ring/ring-json                "0.3.1"]
+                 [compojure                     "1.4.0"]
+                 [cc.qbits/jet                  "0.6.6"]
+                 [ring/ring-json                "0.4.0"]
                  [redis.clients/jedis           "2.6.2"]
                  [org.bouncycastle/bcprov-jdk16 "1.46"]
                  [jumblerg/ring.middleware.cors "1.0.1"]
                  [org.spootnik/logconfig        "0.7.3"]
 
-                 [org.clojure/clojurescript     "0.0-2913"]
-                 [org.omcljs/om                 "0.8.8"]
-                 [secretary                     "1.2.1"]
-                 [racehub/om-bootstrap          "0.4.0"
+                 [org.clojure/clojurescript     "1.7.145"]
+                 [org.omcljs/om                 "0.9.0"]
+                 [secretary                     "1.2.3"]
+                 [racehub/om-bootstrap          "0.5.3"
                   :exclusions [org.clojure/clojure]]
-                 [prismatic/om-tools            "0.3.10"
-                  :exclusions [org.clojure/clojure]]
-                 [cljs-ajax                     "0.3.10"]])
+                 [prismatic/om-tools            "0.3.12"
+                  :exclusions [org.clojure/clojure prismatic/schema]]
+                 [cljs-ajax                     "0.5.0"
+                  :exclusions [commons-codec]]])
